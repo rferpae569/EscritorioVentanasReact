727af1e5d6f34d6b3dc83ffb4e484848debe8bb3
rferpae569 <rferpae569@g.educaand.es>
2024-06-21 00:16:04 +0200

    EscritorioVentanas Ruben Fernandez

--- src/Componentes/Weather.jsx
+++ src/Componentes/Weather.jsx
@@ -18,7 +18,7 @@ export default function Weather(props) {
 						name="Weather"
 						onClick={props.changeHandler}
 					></button>
-					<img src="/EscritorioVentanas/dist/Imagenes/weather.png" />
+					<img src="../../dist/assets/Imagenes/weather.png" />
 				</div>
 			</Draggable>
 		</div>
